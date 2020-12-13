596b2752f62e9cbadb67434e20b9c02fdf696885
gmahima <mahimagangavarapu@gmail.com>
2020-12-13 17:02:18 +0530

    working

--- components/Square.js
+++ components/Square.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, {useMemo} from 'react'
+import {useService} from '@xstate/react'
 import {SquareContainer} from './styled/Square'
 
 const Square = ({s}) => {
+    const [current, send] = useService(s)
     return (
         <SquareContainer>
-            {s.value}
+            {current.context.value}
         </SquareContainer>
     )
 }
