4f32c9c60cd93829979cef31982f078313bdb11f
gmahima <mahimagangavarapu@gmail.com>
2020-12-21 13:46:47 +0530

    conditionally return toast

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,61 @@ import styled, {css} from 'styled-components'
 import tw from 'twin.macro'
 import {AppContainer, GameContainer} from '../components/styled/index'
 import Game from '../components/Game'
-
+import {motion, AnimatePresence} from 'framer-motion'
+import React, {useState} from 'react'
+const toastVariants= {
+  initial: {y: "200%"},
+  animate: {y: "0%"},
+  exit: {y: "200%"}
+}
+const spanVariants= {
+  initial: {color: '#ff0000'},
+  animate: {color: '#00ff00'},
+  exit: {color: '#0000ff'}
+}
+const blueSpanVariants= {
+  initial: {color: '#00ff00'},
+  animate: {color: '#0000ff'},
+  exit: {color: '#ff0000'}
+}
+function Toast ({blue}) {
+  if(blue) {
+    return (
+      <motion.div css={tw`bg-gray-900 text-white py-2 px-4 bg-opacity-75 m-2 rounded absolute bottom-0 right-0`}
+    variants={toastVariants}
+    initial="initial"
+    animate="animate"
+    exit="exit"
+    >
+      <motion.span variants={spanVariants}>toast</motion.span>
+    </motion.div>
+    )
+  }
+  else {
+    return (
+      <motion.div css={tw`bg-gray-900 text-white py-2 px-4 bg-opacity-75 m-2 rounded absolute bottom-0 right-0`}
+    variants={toastVariants}
+    initial="initial"
+    animate="animate"
+    exit="exit"
+    >
+      <motion.span variants={blueSpanVariants}>blue toast</motion.span>
+    </motion.div>
+    )
+  }
+}
 export default function Home() {
+  const [showToast, setShowToast] = useState(false)
   return (
     <AppContainer>
       <GameContainer>
         <Game />
       </GameContainer>
+      <button onClick={() => {setShowToast(!showToast)}} css={tw`m-2 p-1 bg-green-500 text-white rounded`}>show toast</button>
+      <AnimatePresence>
+      <Toast blue={showToast}></Toast>
+      </AnimatePresence>
+      
     </AppContainer>
   )
 }
